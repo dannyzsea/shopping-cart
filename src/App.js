f0dc95374b5338ef675419f5728bd70297466100
John Doe <javatechisland@gmail.com>
2020-10-14 17:25:47 -0700

    filter.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 import "./App.css";
 
 class App extends Component {
@@ -12,6 +13,20 @@ class App extends Component {
       sort: "",
     };
   }
+  sortProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, product: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availabeSizes.index(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+  filterProducts(size) {}
   render() {
     return (
       <div className="grid-container">
@@ -21,6 +36,13 @@ class App extends Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">CartItem</div>
